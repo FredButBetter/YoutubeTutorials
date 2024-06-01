e4460f46e5254c443fcd9ca0bab8d0577bcdd272
Fred Etingen <63937319+FredButBetter@users.noreply.github.com>
2024-06-01 10:02:28 -0400

    Script for sprite shader, same as video

---
+++ SpriteShader.gd
@@ -0,0 +1,24 @@
+extends Sprite3D
+class_name ShaderSprite3D
+
+var shaderScriptPath : String = "res://OutlineShader3D.gdshader"
+@export var outlineColor := Color.AQUA
+@export var outlineLineWeight := 1.0
+
+func _ready() -> void:
+	_apply_material_override()
+		
+	_apply_texture()
+	texture_changed.connect(_apply_texture)
+	
+func _apply_material_override():
+	var shader_material = ShaderMaterial.new()
+	shader_material.shader = load(shaderScriptPath)
+	material_override = shader_material
+	shader_material.set_shader_parameter("outlineColor", outlineColor)
+	shader_material.set_shader_parameter("lineWeight", outlineLineWeight)
+
+func _apply_texture():
+	var shader_material : ShaderMaterial = material_override
+	shader_material.set_shader_parameter("sprite_texture", texture)
+
